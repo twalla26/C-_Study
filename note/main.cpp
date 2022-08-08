d1ff0ffca2dbf9bdd343bb54d65bfa4855ff81dc
twalla26 <ssm4748@naver.com>
2022-08-09 05:46:20 +0900

    01-3,4,5

--- note/main.cpp
+++ note/main.cpp
@@ -1,37 +1,10 @@
 #include <iostream>
+using namespace std;
 
-void swap(int *a, int *b) {
-    int c = 0;
-    c = *a;
-    *a = *b;
-    *b = c;
-}
+int val = 10; // 전역변수
 
-void swap(char *a, char *b) {
-    char c = 'A';
-    c = *a;
-    *a = *b;
-    *b = c;
-}
-
-void swap(double *a, double *b) {
-    double c = 1.1;
-    c = *a;
-    *a = *b;
-    *b = c;
-}
-
-int main(void) {
-    int num1=20, num2=30;
-    swap(&num1, &num2);
-    std::cout << num1 << ' ' << num2 << std::endl;
-
-    char ch1='A', ch2='Z';
-    swap(&ch1, &ch2);
-    std::cout << ch1 << ' ' << ch2 << std::endl;
-
-    double db11 = 1.111, db12 = 5.555;
-    swap(&db11, &db12);
-    std::cout << db11 << ' ' << db12 << std::endl;
-    return 0;
+int SimpleFunc(void) {
+    int val = 20; // 지역변수
+    val += 3; // 지역변수 val의 값 3 증가
+    ::val += 7; // 전역변수 val의 값 7 증가
 }
